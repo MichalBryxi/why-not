8ee645310df2fdfcd7f9a40c310bbb30fde63bc7
Michal Bryxí <michal.bryxi@gmail.com>
2015-11-08 13:13:43 +0100

    Boat position in isometric mode

--- frontend/lib/display/camera.js
+++ frontend/lib/display/camera.js
@@ -2,9 +2,9 @@ var camera, scene, renderer;
 
 function initCamera (isometric) {
   if (settings.isometricCamera) {
-    var m = 4;
+    var m = 2;
     camera = new THREE.OrthographicCamera( -window.innerWidth*m, window.innerWidth*m, window.innerHeight*m, -window.innerHeight*m, 1, 1000000 );
-    camera.position.set( -1800, 100, 2000 );
+    camera.position.set( -2300, 100, 2000 );
   } else {
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.5, 3000000 );
     camera.position.set( 1000, 750, 1000 );
